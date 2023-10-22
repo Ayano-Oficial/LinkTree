9bd9ba5c9f9ae285b88965a1bf0ddb921854f61f
Ayano <148707134+Ayano-Oficial@users.noreply.github.com>
2023-10-22 14:49:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function Apertou() {
+    let pegaNome = prompt("Parece que você descobriu que aqui tem uma interação com o usuário, me diga seu nome!")
+    
+    document.querySelector("#Bem-vindo").innerHTML = `<span>${pegaNome}</span> Seja bem vindo (a) ao linkTree do Ayano`
+   
+  
+}
